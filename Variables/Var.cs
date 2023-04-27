7aba732d5d1d0b93fa17c01575f6938002a784d3
jrlbln <joerelbelen02@gmail.com>
2023-04-27 10:34:39 +0100

    added Variable types

---
+++ Variables/Var.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Hello World";
+            char grade = 'A';
+            int age = -30;
+            float size = 5.4F;
+            double gpa = 3.3D;
+            decimal price = 10.1M;
+            bool isMale = true;
+
+            Console.ReadLine();
+        }
+    }
+
+}
+
