e98768d612e10d100010695381947d8017ac9428
jrlbln <joerelbelen02@gmail.com>
2023-04-27 12:01:47 +0100

    added var programs

---
+++ Variables/Int.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( 40 );
+
+            //basic int math
+            Console.WriteLine(10 + 10);
+            Console.WriteLine(10 - 10);
+            Console.WriteLine(10 * 10);
+            Console.WriteLine(10 / 10);
+            Console.WriteLine(10 % 8);
+
+            Console.WriteLine();
+
+            //basic deci math
+            Console.WriteLine(0.2 + 9.9);
+            Console.WriteLine(10 - 0.1);
+            Console.WriteLine(0.5 * 0.5);
+            Console.WriteLine(2.2 / 2.1);
+            Console.WriteLine(10.5 % 3.5);
+
+            Console.WriteLine();
+
+            int num = 1;
+            Console.WriteLine(num);
+            num++;
+            Console.WriteLine(num);
+
+            Console.WriteLine();
+
+            //methods
+            Console.WriteLine( Math.Abs(-40) );
+            Console.WriteLine( Math.Pow(3,2) );
+            Console.WriteLine( Math.Sqrt(36) );
+            Console.WriteLine( Math.Max(4, 90) );
+            Console.WriteLine( Math.Min(4, 90) );
+            Console.WriteLine( Math.Round(4.4) );
+            Console.WriteLine( Math.Round(4.7) );
+
+            Console.ReadLine();
+        }
+    }
+
+}
+
---
+++ Variables/strings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Strings
+            Console.WriteLine("Hello \n World");
+            Console.WriteLine("Hello \" World");
+            Console.WriteLine();
+
+            //Concatination
+            string phrase = "Hi " + "Earth";
+            Console.WriteLine( phrase.Length );
+            Console.WriteLine();
+
+            //Method
+            Console.WriteLine( phrase.ToUpper() ) ;
+            Console.WriteLine( phrase.Contains("Hi") );
+            Console.WriteLine( phrase[3] );
+            Console.WriteLine( phrase.IndexOf("Earth"));
+            Console.WriteLine( phrase.IndexOf('r') );
+            Console.WriteLine( phrase.IndexOf('z'));
+            Console.WriteLine( phrase.Substring(3));
+            Console.WriteLine(phrase.Substring(3,3));
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+
+}
+
