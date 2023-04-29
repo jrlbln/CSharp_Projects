ee70e9e252d1d317f1087f80e34c892a4772a677
jrlbln <joerelbelen02@gmail.com>
2023-04-29 08:31:46 +0100

    Create If Calculator.cs

---
+++ Basics/Conditionals/If Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter 1st number: ");
+            double num1=Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter 2nd number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else { Console.WriteLine("Invalid Operator");}
+
+
+            Console.ReadLine();
+        }
+
+
+
+    }
+
+}
+
