e78d4097596e9efb667022203fcef190ddc59d68
jrlbln <joerelbelen02@gmail.com>
2023-04-29 09:29:51 +0100

    Create Switch.cs

---
+++ Basics/Conditionals/Switch.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetDay(1));
+
+            Console.ReadLine();
+        }
+
+        static string GetDay(int dayNum) 
+        {
+            string dayName;
+
+            switch (dayNum) 
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+
+                case 1:
+                    dayName = "Monday";
+                    break;
+
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+
+                case 4:
+                    dayName = "Thursday";
+                    break;
+
+                case 5:
+                    dayName = "Friday";
+                    break;
+
+                case 6:
+                    dayName = "Saturday";
+                    break;
+
+                default:
+                    dayName = "Invalid Day Number";
+                    break;
+            }
+
+            return dayName;
+        }
+
+    }
+
+}
+
