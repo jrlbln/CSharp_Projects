0ad04df4e0de47aad89c317e94ddee3509c9af8c
jrlbln <joerelbelen02@gmail.com>
2023-05-02 07:56:53 +0100

    Create Methods.cs

---
+++ Basics/Methods/Methods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Tom" , 33);
+            SayHi("Joe" , 56);
+            SayHi("Mike" , 12);
+
+            Console.ReadLine();
+        }
+
+        static void SayHi(string name, int age) 
+        {
+            Console.WriteLine("Hello " + name + " you are " + age + " years old ");
+        }
+    }
+
+}
+
