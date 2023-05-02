263387b227595647d6a92fb4c977dcde3ad5a717
jrlbln <joerelbelen02@gmail.com>
2023-05-02 07:43:52 +0100

    Create Exponent Method.cs

---
+++ Basics/Methods/Exponent Method.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetPow(-3, 3));
+
+            Console.ReadLine();
+        }
+
+        static int GetPow(int baseNum, int powNum)
+        {
+            int result = 1;
+
+            for (int i = 0; i < powNum; i++)
+            { 
+                result *= baseNum;
+            }
+
+            return result;
+        }
+
+    }
+
+}
+
