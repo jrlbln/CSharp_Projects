2fd6130d3707f71c50ed6a9382a98a2ae070ec13
jrlbln <joerelbelen02@gmail.com>
2023-05-02 08:29:23 +0100

    Create Return Statement.cs

---
+++ Basics/Methods/Return Statement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cubedNumber = cube(5);
+
+            Console.WriteLine(cubedNumber);
+
+            Console.ReadLine();
+        }
+
+        static int cube(int num) 
+        {
+            int result = num * num * num;
+            return result;
+        }
+    }
+
+}
+
