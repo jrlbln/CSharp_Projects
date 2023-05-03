f9ffebf216025819d7050110a6b2dc685830d7d3
jrlbln <joerelbelen02@gmail.com>
2023-05-03 09:26:47 +0100

    Create While Loop.cs

---
+++ Basics/Loops/While Loop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+
+            while (index <= 10) 
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+            
+            Console.ReadLine();
+        }
+
+    }
+
+}
+
