a9d7aa9714b2f26e9473b106e7c18685bf617dd3
jrlbln <joerelbelen02@gmail.com>
2023-06-14 16:53:25 +0100

    Create Comments.cs

---
+++ Basics/Comments.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a single line comment
+
+            /* This is a
+             * multi line
+             * comment */
+
+            Console.WriteLine("Comments are fun");
+        }
+
+    }
+
+}
