1cc5f3d183dcf0653816c8f07dd314a76c57e83e
jrlbln <joerelbelen02@gmail.com>
2023-06-15 13:36:16 +0100

    Class and Objects

---
+++ Basics/Classes and Objects/Book.cs
@@ -0,0 +1,16 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    internal class Book
+    {
+        public String title;
+        public String author;
+        public int pages;
+
+    }
+}
---
+++ Basics/Classes and Objects/BookProgram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Book book1 = new Book();
+            book1.title = "Harry Potter";
+            book1.author = "JK Rowling";
+            book1.pages = 400;
+
+            Book book2 = new Book();
+            book2.title = "Lord of the Rings";
+            book2.author = "Tolkein";
+            book2.pages = 700;
+
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book1.author);
+            Console.WriteLine(book1.pages);
+            Console.WriteLine();
+            Console.WriteLine(book2.title);
+            Console.WriteLine(book2.author);
+            Console.WriteLine(book2.pages);
+
+        }
+
+    }
+
+}
