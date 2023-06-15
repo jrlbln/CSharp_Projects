fb0030272ed7cc3fcb79d74b6a9a8d2426519f69
jrlbln <joerelbelen02@gmail.com>
2023-06-15 13:47:31 +0100

    Mid

---
+++ Mid/Constructors/Book.cs
@@ -0,0 +1,23 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    internal class Book
+    {
+        public String title;
+        public String author;
+        public int pages;
+
+        public Book(string aTitle, string aAuthor, int aPages) 
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+
+    }
+}
---
+++ Mid/Constructors/BookProgram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Book book1 = new Book("Harry Potter", "JK Rowling", 400);
+            Book book2 = new Book("Lord of the Rings", "Tolkein", 700);
+
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book1.author);
+            Console.WriteLine(book1.pages);
+            Console.WriteLine();
+            Console.WriteLine(book2.title);
+            Console.WriteLine(book2.author);
+            Console.WriteLine(book2.pages);
+
+        }
+
+    }
+
+}
